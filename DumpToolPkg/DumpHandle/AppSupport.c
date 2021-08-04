11e18d95dff3148aad5c400c575249c0ae80fb59
ESdove <cz18792083952@gmail.com>
2021-08-04 14:37:06 +0800

    Update AppSupport.c

--- DumpToolPkg/DumpHandle/AppSupport.c
+++ DumpToolPkg/DumpHandle/AppSupport.c
@@ -8,63 +8,54 @@
 #include <Library/DebugLib.h>
 #include <Library/UefiApplicationEntryPoint.h>
 
-UINTN  Argc;
-CHAR16 **Argv;
-EFI_SHELL_PROTOCOL      *mShellProtocol = NULL;
+UINTN                Argc;
+CHAR16               **Argv;
+EFI_SHELL_PROTOCOL   *mShellProtocol = NULL;
 
-/**
 
-  This function parse application ARG.
-
-  @return Status
-**/
-EFI_STATUS
-GetArg (
-  VOID
-  )
+EFI_STATUS GetArg (VOID)
 {
   EFI_STATUS                    Status;
   EFI_SHELL_PARAMETERS_PROTOCOL *ShellParameters;
-//   UINTN                         i = 0;
+//UINTN                         i = 0;
 
-  Status = gBS->HandleProtocol (
-                  gImageHandle,
-                  &gEfiShellParametersProtocolGuid,
-                  (VOID**)&ShellParameters
-                  );
-  if (EFI_ERROR(Status)) {
+  Status = gBS->HandleProtocol (gImageHandle,
+                                &gEfiShellParametersProtocolGuid,
+                                (VOID**)&ShellParameters);
+                                
+  if (EFI_ERROR(Status))
+  {
     return Status;
   }
 
   Argc = ShellParameters->Argc;
   Argv = ShellParameters->Argv;
    
-//   for (i = 0; i < Argc; i++)
-
-//   { 
-//       Print(L"Argc = %d\n",Argc);
-//       Print(L"Argv[i] = %s\n",Argv[i]);
-//   }
+// for (i = 0; i < Argc; i++)
+// { 
+//     Print(L"Argc = %d\n",Argc);
+//     Print(L"Argv[i] = %s\n",Argv[i]);
+// }
 
   return EFI_SUCCESS;
 }
 
-EFI_SHELL_PROTOCOL *
-GetShellProtocol (
-  VOID
-  )
-{
-  EFI_STATUS            Status;
 
-  if (mShellProtocol == NULL) {
-    Status = gBS->LocateProtocol (
-                    &gEfiShellProtocolGuid,
-                    NULL,
-                    (VOID **) &mShellProtocol
-                    );
-    if (EFI_ERROR (Status)) {
+
+ EFI_SHELL_PROTOCOL *
+ GetShellProtocol (VOID)
+ {
+   EFI_STATUS            Status;
+
+   if (mShellProtocol == NULL) 
+   {
+     Status = gBS->LocateProtocol (&gEfiShellProtocolGuid,
+                                  NULL,
+                                  (VOID **) &mShellProtocol);
+     if (EFI_ERROR (Status))
+     {
       mShellProtocol = NULL;
-    }
-  }
-  return mShellProtocol;
-}
+     }
+   }
+   return mShellProtocol;
+ }
